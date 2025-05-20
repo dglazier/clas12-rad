829583f34c8f021460c86e760ca21cb1991ffef5
Derek Glazier <Derek.Glazier@glasgow.ac.uk>
2025-05-20 16:35:00 +0100

    refactor CLAS12Reaction to define functions outside of class def, add reverse index function

---
+++ examples/Process_eppippim.C
@@ -0,0 +1,174 @@
+#include "CLAS12DetectorReaction.h"
+#include "ParticleCreator.h"
+#include "Indicing.h"
+#include "Histogrammer.h"
+#include "BasicKinematicsRDF.h"
+#include "ReactionKinematicsRDF.h"
+#include "ElectronScatterKinematicsRDF.h"
+#include "gammaN_2_Spin0Spin0SpinHalfRDF.h"
+#include <TBenchmark.h>
+#include <ROOT/RDataFrame.hxx>
+#include <ROOT/RLogger.hxx>
+#include <chrono>
+
+void Process_eppippim(){
+  ///////////////////////////////////////////////////////////
+  // Some Preliminaries
+  ///////////////////////////////////////////////////////////
+ 
+  using namespace rad::names::data_type; //for Rec(), Truth()
+  // auto verbosity = ROOT::Experimental::RLogScopedVerbosity(ROOT::Detail::RDF::RDFLogChannel(), ROOT::Experimental::ELogLevel::kInfo);  // Log timing etc
+  // ROOT::EnableImplicitMT(4); // run multi-core, needs most recent hipo master
+
+
+  ///////////////////////////////////////////////////////////
+  // Setup files to process
+  ///////////////////////////////////////////////////////////
+  //  auto filename = "~/Jlab/clas12/data/hipo/DVPipPimP_006733.hipo"; //my real data file
+  auto filename = "~/Jlab/clas12/data/simulation/RhoFeb24/rho-7221-9*.hipo"; //my simulated file
+  std::vector<std::string> files = {filename}; //can add as many files as you wish
+  
+  ///////////////////////////////////////////////////////////
+  // Setup RAD dataframe object. Initialise with files
+  ///////////////////////////////////////////////////////////
+  rad::clas12::CLAS12Reaction rf{files};
+  rf.UseFTB(); //Use ForwardTagger based REC::Particle
+  rf.AliasColumnsAndMatchWithMC(); //when using simulated data, mc-match
+  //auto pidtype = "tru_pid";
+
+  //rf.AliasColumns(); //when using real data just use REC::Particles
+  auto pidtype = "rec_pid";
+ 
+  //Set beam energy. Will eventually remove this whn get rcdb interface
+  rf.FixBeamElectronMomentum(0,0,10.4); //default e- mass
+  rf.FixBeamIonMomentum(0,0,0); //default p mass
+
+  
+  ///////////////////////////////////////////////////////////////
+  // Setup final state particles and where to get them
+  // useNthOccurance selects the nth particle of a particular pid
+  // note the final argument is a PID which creates a particle_OK flag
+  // this ==1 if REC::Particle has correct PID ==0 if not
+  ///////////////////////////////////////////////////////////////
+  rf.setScatElectronIndex(rad::indice::useNthOccurance(1,11),{pidtype});//n=1, pid == 11
+  rf.setParticleIndex("pip",rad::indice::useNthOccurance(1,211),{pidtype},211);
+  rf.setParticleIndex("pim",rad::indice::useNthOccurance(1,-211),{pidtype},-211);
+  rf.setParticleIndex("proton",rad::indice::useNthOccurance(1,2212),{pidtype},2212);
+
+
+  ///////////////////////////////////////////////////////////
+  // Create intermediate particles
+  ///////////////////////////////////////////////////////////
+  rf.Particles().Sum("rho",{"pip","pim"}); // rho -> pi+ + pi- 
+
+  ///////////////////////////////////////////////////////////
+  // Set the particles associated with
+  // top (meson) and bottom (baryon)vertices
+  ///////////////////////////////////////////////////////////
+  rf.setBaryonParticles({"proton"}); //recoil proton
+  rf.setMesonParticles({"pip","pim"}); //intermediate meson
+
+  ////  note I could analyse pi- + Delta++ instead
+  // rf.setBaryonParticles({"proton","pip"}); //recoil proton
+  // rf.setMesonParticles({"pim"}); //intermediate meson
+
+  //must call this after all particles are configured
+  rf.makeParticleMap();
+
+  ///////////////////////////////////////////////////////////
+  // For debugging I can output particle info to terminal
+  ///////////////////////////////////////////////////////////
+  //Print mc particles for each event
+  //rad::rdf::PrintParticles(rf);
+  //Print rec particle for each event
+  //rad::rdf::PrintParticles(rf,Rec());
+  
+  ///////////////////////////////////////////////////////////
+  // Create some filter to act on this final state
+  // I have columns for indicing the particles in the record
+  // these are given by the particle names set above
+  // e.g. pip, pim ... also note the scattered electron is scat_ele
+  ///////////////////////////////////////////////////////////
+  //Minimum momentum cut on reconstructed particles
+  //note columns rec_pmag, rec_theta, rec_phi have been created by default
+  //I use the particle index to access its value
+  rf.Filter("(rec_pmag[scat_ele]>0.1)*(rec_pmag[pip]>0.5)*(rec_pmag[pim]>0.5)*(rec_pmag[proton]>0.1)","rec_cut");
+  //EB PID cut on all particles
+  rf.Filter("(scat_ele_OK==1) *(pip_OK==1) * (pim_OK==1) * (proton_OK==1)","pid_cut");
+  
+  ///////////////////////////////////////////////////////////
+  // PErform some kinematic calculations.
+  // These come from predefined functions in RAD header files
+  // I can also add my own header and functions via a #include
+  // at the top of this macro
+  ///////////////////////////////////////////////////////////
+  //masses column name, {+ve particles}, {-ve particles}
+  // #include "BasicKinematicsRDF.h"
+  rad::rdf::MissMass(rf,"W","{scat_ele}");
+  rad::rdf::Mass(rf,"Whad","{rho,proton}");
+  rad::rdf::Mass(rf,"RhoMass","{rho}");
+
+  //t distribution, column name
+  //#include "ReactionKinematicsRDF.h"
+  rad::rdf::TBot(rf,"tb");
+  rad::rdf::TPrimeBot(rf,"tbp");
+  rad::rdf::TTop(rf,"tt");
+  rad::rdf::TPrimeTop(rf,"ttp");
+
+  //CM production angles
+  rad::rdf::CMAngles(rf,"CM");
+  rad::rdf::Q2(rf,"Q2");
+
+  //decay angles
+  // #include "gammaN_2_Spin0Spin0SpinHalfRDF.h"
+  rad::rdf::gn2s0s0s12::HelicityAngles(rf,"Heli");
+
+  ///////////////////////////////////////////////////////////
+  // Define histograms using rad::histo::Histogrammer
+  // Will create histograms for Rec and Truth variables
+  // can split in many kinemtic bins and have histo for each bin
+  ///////////////////////////////////////////////////////////
+  rad::histo::Histogrammer histo{"set1",rf};
+  // we can create many histograms by splitting events into
+  // bins, where the same histogram is produced for the given bin
+  // e.g. create 10 bins in tru_W between 4 and 54 GeV 
+  // histo.Splitter().AddRegularDimension(Truth()+"W", rad::histo::RegularSplits(10,4,14) );
+  
+  histo.Init({Rec(),Truth()});//will create same histograms for rec and truth variables
+
+  //just got to watch the type given for each variable
+  //if you get it wrong it will complain at run time
+  //and tell you which type you should have used 
+  histo.Create<TH1D,double>({"Q2","Q2",500,0,5},{"Q2"});
+  histo.Create<TH1D,double>({"W","W",100,0,20.},{"W"});
+  histo.Create<TH1D,double>({"RhoMass","M(2#pi) [GeV]",100,0,3},{"RhoMass"});
+  histo.Create<TH1D,double>({"tb","t(p,p') [GeV^{2}]",100,-2,5},{"tb"});
+  histo.Create<TH1D,double>({"tt","t(g,X) [GeV^{2}]",100,-2,5},{"tt"});
+  histo.Create<TH1D,double>({"cthCM","cos(#theta_{CM})",100,-1,1},{"CM_CosTheta"});
+  histo.Create<TH1D,double>({"phCM","#phi_{CM})",100,-TMath::Pi(),TMath::Pi()},{"CM_Phi"});
+  histo.Create<TH1D,float>({"cthHeli","cos(#theta_{hel})",100,-1,1},{"Heli_CosTheta"});
+  histo.Create<TH1D,float>({"phCHeli","#phi_{hel})",100,-TMath::Pi(),TMath::Pi()},{"Heli_Phi"});
+  histo.Create<TH1D,float>({"EleP","p_{e}",100,0,20},{"pmag[scat_ele]"});
+
+  //create another histogrammer for resolutions
+  rad::histo::Histogrammer histo_res{"res",rf};
+  histo_res.Init();// just create untyped histograms (i.e. will not append Rec or Truth, you must do this)
+  histo_res.Create<TH1D,float>({"resEleP","#Delta P_{e'}",100,-1,1},{"res_pmag[scat_ele]"});
+  histo_res.Create<TH2D,float,float>({"PVresEleP","P_{e'} v #Delta P_{e'}",100,-1,1,100,0,20},{"res_pmag[scat_ele]","rec_pmag[scat_ele]"});
+
+  ///////////////////////////////////////////////////////////
+  // Process by saving all histograms to file
+  ///////////////////////////////////////////////////////////
+  //save all histograms to file
+  histo.File("histos/eppippim_histos.root");
+  histo_res.File("histos/eppippim_res_histos.root");
+ 
+
+  ///////////////////////////////////////////////////////////
+  // Process by saving all columns to a tree
+  ///////////////////////////////////////////////////////////
+  //save tree with all defined branches
+  rf.Snapshot("trees/det_eppippim_trees.root");
+
+  
+}
--- include/CLAS12Reaction.h
+++ include/CLAS12Reaction.h
@@ -13,75 +13,207 @@
 #include "hipo4/RHipoDS.hxx"
 
 namespace rad{
-  namespace config{
+  namespace clas12 {
     using rad::names::data_type::Rec;
     using rad::names::data_type::Truth;
 
     //! Class definition
 
-    class CLAS12Reaction : public ElectroIonReaction {
+    class CLAS12Reaction : public rad::config::ElectroIonReaction {
 
- 
+     
 
     public:
 
       CLAS12Reaction(const std::string_view fileNameGlob ) :
-      ElectroIonReaction{ ROOT::RDataFrame{std::move(std::make_unique<RHipoDS>(fileNameGlob))} } {
-	/* auto ds  = std::make_unique<RHipoDS>(fileNameGlob); */
-        /* auto total_events = ds->GetEntries(); */
- 
-	/* auto rdf = ROOT::RDataFrame{std::move(ds)}; */
-	/* setBaseFrame(rdf); */
-	/* setCurrFrame(rdf); */
-	/*   std::cout<<"CLAS12Reaction "<<CurrFrame().GetNFiles()<<" "<<rdf.GetNFiles()<<" "<<total_events<<std::endl; */
+	rad::config::ElectroIonReaction{ ROOT::RDataFrame{std::move(std::make_unique<RHipoDS>(fileNameGlob))} } {
+
       }
-    CLAS12Reaction(const std::vector<std::string> &filenames ) : ElectroIonReaction{ROOT::RDataFrame{std::move(std::make_unique<RHipoDS>(filenames))}} {
-	std::cout<<"CLAS12Reaction "<<CurrFrame().GetNFiles()<<std::endl;
+
+      CLAS12Reaction(const std::vector<std::string> &filenames ) : rad::config::ElectroIonReaction{ROOT::RDataFrame{std::move(std::make_unique<RHipoDS>(filenames))}} {
  
       }
 
+      void AliasColumns(Bool_t IsEnd=kTRUE);
+      void AliasColumnsFTB(Bool_t IsEnd=kTRUE);
+      void AliasColumnsMC(Bool_t IsEnd=kTRUE);
+      void AliasColumnsAndMC(Bool_t IsEnd=kTRUE);
+      void AliasColumnsAndMatchWithMC(Bool_t IsEnd=kTRUE);
+      void PostParticles() override;
+      void AddAdditionalComponents();
+      template<typename T> 
+      void RedefineFundamental( const string& name );
+
+ 
+
+      void UseFTB(){_isFTBased=true;}
+      
+      bool IsTruthMatched()const {return _truthMatched;}
+
+    private:
+
+      bool _isFTBased=false;     
+      bool _truthMatched =false;
+     
+    };//CLAS12Reaction
+
+
+      /////////Class method implementations below
       /**
        * Only alias REC::Particles columns
        */ 
-       void AliasColumns(Bool_t IsEnd=kTRUE){
-	AddType(Rec());
+    void CLAS12Reaction::AliasColumns(Bool_t IsEnd){
+      if(_isFTBased) return AliasColumnsFTB();
+	 
+      AddType(Rec());
 
-	 setBranchAlias("REC_Particle_px",Rec()+"px");
-	 setBranchAlias("REC_Particle_py",Rec()+"py");
-	 setBranchAlias("REC_Particle_pz",Rec()+"pz");
-	 setBranchAlias("REC_Particle_pid",Rec()+"pid");
-	 //create a column for particle masses
-	 Define(Rec()+"m",rad::clas12::AssignMasses,{Rec()+"pid"});
+      setBranchAlias("REC_Particle_px",Rec()+"px");
+      setBranchAlias("REC_Particle_py",Rec()+"py");
+      setBranchAlias("REC_Particle_pz",Rec()+"pz");
+      setBranchAlias("REC_Particle_pid",Rec()+"pid");
+      //create a column for particle masses
+      Define("REC_Particle_m",rad::clas12::AssignMasses,{Rec()+"pid"});
+      //need to alias it for redefines
+      setBranchAlias("REC_Particle_m",Rec()+"m");
 
-	 //reaction::util::CountParticles(this,Rec());
-       }
+      setBranchAlias("REC_Particle_status",Rec()+"status");
+      setBranchAlias("REC_Particle_vt",Rec()+"vt");
+      setBranchAlias("REC_Particle_vx",Rec()+"vx");
+      setBranchAlias("REC_Particle_vy",Rec()+"vy");
+      setBranchAlias("REC_Particle_vz",Rec()+"vz");
+      setBranchAlias("REC_Particle_beta",Rec()+"beta");
+      setBranchAlias("REC_Particle_chi2pid",Rec()+"chi2pid");
 
+      //Make a list of good particles
+      //INPROCESS
+      // Define("REC_Particle_good"[](){});
+	 
+      reaction::util::CountParticles(this,Rec());
+    }
+    /**
+     * Only alias RECFT::Particles columns
+     */ 
+    void CLAS12Reaction::AliasColumnsFTB(Bool_t IsEnd){
+      //Actually would like to switch to
+      //REC::Particle if no RECFT entry, i.e. no FT electron
+      //Could make intermediate Defines instead,
+      //returning FTB by default
+      //and normal if no FT electron
+      //then alias the defines to rec_px etc
+	 
+      AddType(Rec());
+      
+      Define(Rec()+"n",[](const ROOT::RVecD& px){return px.size();},{"RECFT_Particle_px"});
+   
+      setBranchAlias("RECFT_Particle_px",Rec()+"px");
+      setBranchAlias("RECFT_Particle_py",Rec()+"py");
+      setBranchAlias("RECFT_Particle_pz",Rec()+"pz");
+      setBranchAlias("RECFT_Particle_pid",Rec()+"pid");
+      //create a column for particle masses
+      Define("RECFT_Particle_m",rad::clas12::AssignMasses,{Rec()+"pid"});
+      //need to alias it for redefines
+      setBranchAlias("RECFT_Particle_m",Rec()+"m");
 
-      void PostParticles() override{
-	//once particle are added to vectors
-	//we can calculate additional components
-	AddAdditionalComponents();
+      setBranchAlias("RECFT_Particle_status",Rec()+"status");
+      setBranchAlias("RECFT_Particle_vt",Rec()+"vt");
+      setBranchAlias("REC_Particle_vx",Rec()+"vx");
+      setBranchAlias("REC_Particle_vy",Rec()+"vy");
+      setBranchAlias("REC_Particle_vz",Rec()+"vz");
+      setBranchAlias("RECFT_Particle_beta",Rec()+"beta");
+      setBranchAlias("RECFT_Particle_chi2pid",Rec()+"chi2pid");
 
-	/* if(IsTruthMatched()){ */
-	/*   //add resolution functions */
-	/*   ResolutionFraction<float>("pmag"); */
-	/*   Resolution("theta"); */
-	/*   Resolution("phi"); */
-	/*   Resolution("eta"); */
+      //Make a list of good particles
+      //INPROCESS
+      // Define("REC_Particle_good"[](){});
+    
+      reaction::util::CountParticles(this,Rec());
+    }
 
-	/* } */
+    /**
+     * Only alias MC::Lund columns
+     */ 
+    void CLAS12Reaction::AliasColumnsMC(Bool_t IsEnd){
+      AddType(Truth());
+	
+      setBranchAlias("MC_Lund_px",Truth()+"px");
+      setBranchAlias("MC_Lund_py",Truth()+"py");
+      setBranchAlias("MC_Lund_pz",Truth()+"pz");
+      setBranchAlias("MC_Lund_pid",Truth()+"pid");
+      setBranchAlias("MC_Lund_mass",Truth()+"m");
+	
+      Define(Truth()+"n",Form("rad::helpers::Count(MC_Lund_type,static_cast<short>(1))") );
+    }
+    /**
+     * Alias ReconstructedParticles and MCParticle columns
+     */ 
+    void CLAS12Reaction::AliasColumnsAndMC(Bool_t IsEnd){
+      AliasColumns(kFALSE);
+      AliasColumnsMC(kFALSE);
+
+      //columns for mc matching
+      setBranchAlias("MC_GenMatch_pindex",Rec()+"match_id");
+      setBranchAlias("MC_GenMatch_mcindex",Truth()+"match_id");
+      setBranchAlias("MC_GenMatch_quality",Truth()+"qual");
+    }
+    /**
+     * Alias the columns and rearrange entries 
+     * according to MC::Match
+     * this reorders reconstructed to match mc
+     */
+    void CLAS12Reaction::AliasColumnsAndMatchWithMC(Bool_t IsEnd){
+
+      AliasColumnsAndMC(kFALSE);
+      _truthMatched = true;
+	
+      if(IsEnd){
+	reaction::util::RedefineFundamentalAliases(this);
+
       }
-      void AddAdditionalComponents(){
-	 //and add some additional columns
-	 DefineForAllTypes("phi", Form("rad::ThreeVectorPhi(components_p3)"));
-	 DefineForAllTypes("theta", Form("rad::ThreeVectorTheta(components_p3)"));
-	 DefineForAllTypes("pmag", Form("rad::ThreeVectorMag(components_p3)"));
-	 
-	 Filter([](const ROOT::RVecF& rth,const ROOT::RVecF& rpmag,const ROOT::RVecF& rph){return true;},{Rec()+"pmag",Rec()+"theta",Rec()+"phi"});
-	 
-       }
-       
-    };//CLAS12Reaction
+    }
+    /**
+     * Particles have been reordered etc, ready to do some calculations
+     * here we add some useful momentum components and resolutions
+     */
+    void CLAS12Reaction::PostParticles() {
+      //once particle are added to vectors
+      //we can calculate additional components
+      AddAdditionalComponents();
 
-  }
-}
+      if(IsTruthMatched()){
+	reaction::util::ResolutionFraction(this,"pmag");
+	reaction::util::Resolution(this,"theta");
+	reaction::util::Resolution(this,"phi");
+      }
+    }
+    /**
+     * Calculate spherical momentum components
+     */
+    void CLAS12Reaction::AddAdditionalComponents(){
+      //and add some additional columns
+      DefineForAllTypes("phi", Form("rad::ThreeVectorPhi(components_p3)"));
+      DefineForAllTypes("theta", Form("rad::ThreeVectorTheta(components_p3)"));
+      DefineForAllTypes("pmag", Form("rad::ThreeVectorMag(components_p3)"));
+	 
+	 
+    }
+
+    /**
+     * Reorder REC::Particles to match MC::Lund
+     */
+    template<typename T> 
+    void CLAS12Reaction::RedefineFundamental( const string& name ){
+	
+      auto contains = [](const std::string&  s1,const std::string& s2){
+	return (s1.find(s2) != std::string::npos);
+      };
+      if(contains(name,Rec()+"match_id") ){return;}//don't reorder our order!
+      if(contains(name,Truth()+"match_id") ){return;}//don't reorder our order!
+	  
+      if(contains(name,Rec()) ){
+	RedefineViaAlias(name,helpers::Reorder<T,short,short>,{name.data(),Rec()+"match_id",Truth()+"match_id",Truth()+"n"});
+      }
+
+    }
+      
+  }//config
+}//rad
